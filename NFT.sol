8531747e6cf623d224ff5d5f51ccd7fb03a4a369
GrandF17 <88710236+GrandF17@users.noreply.github.com>
2022-04-07 20:15:48 +0300

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -11,7 +11,6 @@ contract NFT is ERC721Enumerable, Ownable {
 
     string          baseURI = "https://bit-rush.com/punkins/";
     address public  addr_NFT_owner;
-    address public  addr_NFT_buyer;
     string  public  baseExtension = ".json";
 
     // uint256 public  tokensPerEth = 100;         //ERC20 costs 0.01 eth
@@ -23,14 +22,10 @@ contract NFT is ERC721Enumerable, Ownable {
     bool    public  paused = false;
 
     constructor(
-        address _addr_contr,
-        address _addr_NFT_owner,
-        address _addr_NFT_buyer
+        address _address_of_ERC20_contract
     ) ERC721("V", "V") {
-        require(msg.sender == _addr_NFT_owner);
-        token = YourToken(_addr_contr);
+        token = YourToken(_address_of_ERC20_contract);
         setOwner(msg.sender);
-        addr_NFT_buyer = _addr_NFT_buyer;
     }
 
     // internal
@@ -44,10 +39,11 @@ contract NFT is ERC721Enumerable, Ownable {
 
     function buyTokens(uint256 amountToBuy) public returns (uint256 tokenAmount) {
         uint256 allCost = amountToBuy * cost;
-        console.log(allCost);
-        require(allCost <= token.balanceOf(addr_NFT_buyer), "Not enough balance of ERC20");
+        // console.log(allCost);
 
-        token.transferFrom(addr_NFT_buyer, addr_NFT_owner, allCost);
+        require(allCost <= token.balanceOf(msg.sender), "Not enough balance of ERC20");
+
+        token.transferFrom(msg.sender, addr_NFT_owner, allCost);
         mint(amountToBuy);
 
         return amountToBuy;
@@ -61,8 +57,8 @@ contract NFT is ERC721Enumerable, Ownable {
         require(_mintAmount <= maxMintAmount);
         require(supply + _mintAmount <= maxSupply);
 
-        if (addr_NFT_buyer != owner()) {
-            require(token.balanceOf(addr_NFT_buyer) >= cost * _mintAmount);
+        if (msg.sender != owner()) {
+            require(token.balanceOf(msg.sender) >= cost * _mintAmount);
         }
 
         for (uint256 i = 1; i <= _mintAmount; i++) {
@@ -71,9 +67,93 @@ contract NFT is ERC721Enumerable, Ownable {
     }
 
     //only owner
-    /*function setCost(uint256 _newCost) public onlyOwner {
-        costOfOneNFTinToken = _newCost;
-    }*/
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
+
+    function setOwner(address _owner) public onlyOwner {
+        addr_NFT_owner = _owner;
+    }
+
+    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+        maxMintAmount = _newmaxMintAmount;
+    }
+
+    function pause(bool _state) public onlyOwner {
+        paused = _state;
+    }
+}// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./YourToken.sol";
+
+contract NFT is ERC721Enumerable, Ownable {
+    YourToken token;
+
+    using Strings for uint256;
+
+    string          baseURI = "https://bit-rush.com/punkins/";
+    address public  addr_NFT_owner;
+    string  public  baseExtension = ".json";
+
+    // uint256 public  tokensPerEth = 100;         //ERC20 costs 0.01 eth
+    // uint256 public  costOfOneNFTinToken = 2;    //NFT costs 2 ERC20
+    uint256 public  cost = 1;                   //NFT costs 0.02 ETH
+
+    uint256 public  maxSupply = 10000;
+    uint256 public  maxMintAmount = 20;
+    bool    public  paused = false;
+
+    constructor(
+        address _address_of_ERC20_contract
+    ) ERC721("V", "V") {
+        token = YourToken(_address_of_ERC20_contract);
+        setOwner(msg.sender);
+    }
+
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function ballance() public view returns (uint256) {
+        return token.totalSupply();
+    }
+
+    function buyTokens(uint256 amountToBuy) public returns (uint256 tokenAmount) {
+        uint256 allCost = amountToBuy * cost;
+        // console.log(allCost);
+
+        require(allCost <= token.balanceOf(msg.sender), "Not enough balance of ERC20");
+
+        token.transferFrom(msg.sender, addr_NFT_owner, allCost);
+        mint(amountToBuy);
+
+        return amountToBuy;
+    }
+
+    // public
+    function mint(uint256 _mintAmount) public {
+        uint256 supply = totalSupply();
+        require(!paused);
+        require(_mintAmount > 0);
+        require(_mintAmount <= maxMintAmount);
+        require(supply + _mintAmount <= maxSupply);
+
+        if (msg.sender != owner()) {
+            require(token.balanceOf(msg.sender) >= cost * _mintAmount);
+        }
+
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+    //only owner
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
 
     function setOwner(address _owner) public onlyOwner {
         addr_NFT_owner = _owner;
